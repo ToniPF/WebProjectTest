b6e5ad8e98590297e9f69b232bc75a765474588f
ToniPF <toni.pafe@gmail.com>
2019-02-12 10:57:44 +0100

    Downloading html and creating a tree

---
+++ web/clienthttp.py
@@ -0,0 +1,36 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8:
+
+from urllib.request import urlopen
+import bs4
+
+
+class ClientWeb(object):
+    """Client web per la web de la EPS."""
+    def __init__(self):
+        super(ClientWeb, self).__init__()
+
+    def descarregar_html(self):
+        f = urlopen("http://www.eps.udl.cat/ca/")
+        html = f.read()
+        f.close()
+        return html
+
+    def buscar_activitats(self, html):
+        arbre = bs4.BeautifulSoup(html, features="lxml")
+        activitats = arbre.find_all("div", "featured-links-item")
+        return activitats
+
+    def run(self):
+        # descarregar-me html
+        html = self.descarregar_html()
+        # buscar activitats
+        activitats = self.buscar_activitats(html)
+        # imprimir resultats
+        print(activitats)
+
+
+if __name__ == "__main__":
+    c = ClientWeb()
+    c.run()
